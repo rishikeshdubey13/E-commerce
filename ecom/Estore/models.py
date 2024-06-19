bda65b2b870d3c1392506f879af3a91c9a34acae
Rishikesh Dubey <rishikeshdubey942@gmail.com>
2024-06-19 22:28:21 +0100

    models Created
    Created basic models structure

--- ecom/Estore/models.py
+++ ecom/Estore/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+#category of prdocts
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+ #customers
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f' {self.first_name} {self.last_name}'
+
+
+#all products
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default = 1)
+    description = models.CharField(max_length=1000,default = '',blank=True, null=True)
+    image = models.ImageField(upload_to='upload/product/')
+
+    def __str__(self):
+        return self.name
+
+
+
+#customer orders
+class Order(models.Model):
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete= models.CASCADE)
+    quantity  = models.IntegerField(default=1)
+    # price = models.DecimalField()
+    address = models.CharField(max_length =100)
+    phone = models.CharField(max_length=11)
+    date = models.DateField(default = datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
+
+
+
+#Customer Wishlist
+#class Wishlist(models.Model):
+
+
