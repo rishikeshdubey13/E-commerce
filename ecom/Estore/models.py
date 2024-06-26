f241388994d3bc986d03352096814480b3ba31ee
Rishikesh Dubey <rishikeshdubey942@gmail.com>
2024-06-26 23:14:56 +0100

    home page
    Created a basic home page, with login page and urls and managed login and logout views

--- ecom/Estore/models.py
+++ ecom/Estore/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
 
  #customers
 class Customer(models.Model):
@@ -31,6 +34,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE,default = 1)
     description = models.CharField(max_length=1000,default = '',blank=True, null=True)
     image = models.ImageField(upload_to='upload/product/')
+    is_sale = models.BooleanField(default = False)
+    sale_price = models.DecimalField(default = 0 ,decimal_places= 2, max_digits=7)
 
     def __str__(self):
         return self.name
