d2afdddc4b82f0ce065f7d4d04c742eb8316c240
Rishikesh Dubey <rishikeshdubey942@gmail.com>
2024-08-06 14:24:56 +0100

    updated cart quantity itme
    Added quantity where you can select quantity of item

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -1,5 +1,7 @@
+from Estore.models import Product
+
 class Cart():
-    def __init__(self, request):
+    def __init__(self, request):#initializes the cart   
         self.session = request.session
 
         #GEt the current session key if exists:
@@ -11,3 +13,33 @@ class Cart():
 
         # make sure that cart is available on the pages of the website
         self.cart = cart
+
+    
+    def add(self, product,quantity):#allows to add things to cart
+        product_id = str(product.id)
+        product_qty =str(quantity)
+
+        #logic
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+
+        
+        self.session.modified = True
+
+    def __len__(self):#returns the length of the cart
+        return len(self.cart)
+    
+    def get_prods(self): #Lookup stuff in the cart
+        #get the product IDs from the cart
+        product_ids = self.cart.keys()
+        #use ids to lookup product in database
+        products= Product.objects.filter(id__in = product_ids)
+        #return looked up products
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from Estore.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 
 def cart_details(request):
-    return render(request,'cart_details.html')
+
+    #get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    quantities = cart.get_quants()
+    return render(request,'cart_details.html',{"cart_products":cart_products, "quantities":quantities})
 
 
 def cart_add(request):
-    pass
+
+    #get the cart
+    cart = Cart(request)
+
+    #test for the post 
+    if request.POST.get('action') == 'post': #here the (action)'post' is for jquery used in product.html  || while the POST is for the webpage django understands.  
+
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        #lookup product in models(db)
+        product = get_object_or_404(Product, id= product_id)
+
+        #Save to session
+        cart.add(product=product,quantity =product_qty )
+
+        cart_quantity =  cart.__len__()
+
+        # response = JsonResponse({'Product Name': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        return response
+    
 
 def cart_delete(request):
     pass
