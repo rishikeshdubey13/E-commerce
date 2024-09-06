913d0d83234de18851f2a9411f121afb175169f5
Rishikesh Dubey <rishikeshdubey942@gmail.com>
2024-09-06 02:03:59 +0100

    profile model
    Made an extended user profile with some extra user details

--- ecom/Estore/admin.py
+++ ecom/Estore/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order,Profile
+from django.contrib.auth.models import User
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+#Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+
+#Extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ['username', 'first_name', 'last_name', 'email']
+
+    inlines = [ProfileInline]
+
+#in django unregister and register again
+
+#unregister
+admin.site.unregister(User)
+
+#register
+admin.site.register(User, UserAdmin)
--- ecom/Estore/models.py
+++ ecom/Estore/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+
+#Create customer profile
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    date_mdodified = models.DateTimeField(auto_now =True)
+    phone= models.CharField(max_length =20, blank =True)
+    address1 = models.CharField(max_length =200, blank =True)
+    address2= models.CharField(max_length =200, blank =True)
+    city  = models.CharField(max_length =20, blank =True)
+    state = models.CharField(max_length =20, blank =True)
+    zipcode = models.CharField(max_length =20, blank =True)
+    country = models.CharField(max_length =20, blank =True)
+
+    def __str__(self):
+        return self.user.username
+    
+# create  a profile for user when they register
+def create_profile(sender,instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+#automate the Prfile creation
+post_save.connect(create_profile,sender = User)
+
 
 
 #category of prdocts
